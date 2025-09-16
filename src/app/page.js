e6c6de12d51539d4c0e440be1ef5a8865dc8c5e2
tintin4303 <nyunt.tin4303@gmail.com>
2025-09-16 23:47:13 +0700

    finish navbar

--- src/app/page.js
+++ src/app/page.js
@@ -4,22 +4,28 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col p-5 px-10">
       <nav>
-        <div className="flex justify-between nav-bar px-5">
-          <div>
+        <div className="flex justify-between nav-bar px-5 glass-effect">
+          <div className="transition-transform duration-200 hover:scale-110 font-bold">
             <a href="/">StudyFlash</a>
           </div>
           <div className="flex gap-5">
-            <div className="nav-item">
+            <div className="nav-item glass-effect px-3 transition-transform duration-200 hover:scale-110">
               <a href="/flashcards">My Flashcards</a>
             </div>
-            <div className="nav-item">
+            <div className="nav-item glass-effect px-3 transition-transform duration-200 hover:scale-110">
               <a href="/practice">Practice</a>
             </div>
           </div>
         </div>
       </nav>
-      <div>Hello </div>
-      <footer>bye</footer>
+
+      <div className="grid p-10 grid-cols-[3fr_1fr]">
+          <div className="bg-gray-200">Main</div>
+          <div className="bg-gray-400">Side</div>
+      </div>
+      <footer>
+        bye
+      </footer>
     </div>
   );
 }
