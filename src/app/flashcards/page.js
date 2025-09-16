8c015ccf44d691f41e62203bc27333f7e38eac23
tintin4303 <nyunt.tin4303@gmail.com>
2025-09-17 00:32:13 +0700

    finish home page v1 and added routes

---
+++ src/app/flashcards/page.js
@@ -0,0 +1,3 @@
+export default function FlashcardsPage() {
+  return <div>Flashcards Content</div>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col p-5 px-10">
       <nav>
         <div className="flex justify-between nav-bar px-5 glass-effect">
-          <div className="transition-transform duration-200 hover:scale-110 font-bold">
-            <a href="/">StudyFlash</a>
+          <div className="text-2xl font-roboto-mono">
+            <Link href="/">StudyFlash</Link>
           </div>
           <div className="flex gap-5">
-            <div className="nav-item glass-effect px-3 transition-transform duration-200 hover:scale-110">
-              <a href="/flashcards">My Flashcards</a>
+            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110">
+              <Link href="/flashcards">My Flashcards</Link>
             </div>
-            <div className="nav-item glass-effect px-3 transition-transform duration-200 hover:scale-110">
-              <a href="/practice">Practice</a>
+            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110">
+              <Link href="/practice">Practice</Link>
             </div>
           </div>
         </div>
       </nav>
 
-      <div className="grid p-10 grid-cols-[3fr_1fr]">
-          <div className="bg-gray-200">Main</div>
-          <div className="bg-gray-400">Side</div>
+      <div className="">
+          <div className="px-5">
+            <div className="text-4xl font-roboto-mono font-bold mb-5 mt-10">
+              Create your own flash cards and practice for better grades!
+            </div>
+            <div className="btn glass-effect px-10 py-3 w-fit text-lg font-semibold transition-transform duration-200 hover:scale-110">
+              <Link href="/flashcards">Get Started</Link>
+            </div>
+          </div>
       </div>
       <footer>
-        bye
+        <div className="mt-auto text-center py-5 font-roboto-mono">
+          &copy; 2024 StudyFlash. AU Project.
+        </div>
       </footer>
     </div>
   );
