c75048f7b51e3181978beddef29b43aeb917b731
tintin4303 <nyunt.tin4303@gmail.com>
2025-09-17 03:40:52 +0700

    fix navbar responsive

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col p-5 px-10">
-      <nav>
         <div className="flex justify-between nav-bar px-5 glass-effect">
           <div className="text-2xl font-roboto-mono">
             <Link href="/">StudyFlash</Link>
           </div>
           <div className="flex gap-5">
-            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110">
+            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110 hidden sm:block">
               <Link href="/flashcards">My Flashcards</Link>
             </div>
             <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110">
@@ -18,7 +17,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </nav>
 
       <div className="">
           <div className="px-5">
