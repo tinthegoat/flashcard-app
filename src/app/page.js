c162c7335097a69864af7ae6d818dcc8079559da
tintin4303 <nyunt.tin4303@gmail.com>
2025-09-17 04:00:38 +0700

    change background to gradient

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col p-5 px-10">
+    // Navbar
+    <div className="flex min-h-screen flex-col p-5 bg-gradient-to-b from-black via-blue-900 to-purple-900">
         <div className="flex justify-between nav-bar px-5 glass-effect">
           <div className="text-2xl font-roboto-mono">
             <Link href="/">StudyFlash</Link>
@@ -18,16 +19,27 @@ export default function Home() {
           </div>
         </div>
 
+      {/* Body */}
       <div className="">
           <div className="px-5">
             <div className="text-4xl font-roboto-mono font-bold mb-5 mt-10">
-              Create your own flash cards and practice for better grades!
+              Create your own flash cards and practice for better grades.
+              <div className="text-lg mt-3 font-normal">
+                Try it now!
+              </div>
             </div>
             <div className="btn glass-effect px-10 py-3 w-fit text-lg font-semibold transition-transform duration-200 hover:scale-110">
               <Link href="/flashcards">Get Started</Link>
             </div>
           </div>
       </div>
+
+      {/* Recent Cards */}
+      <div className="mt-15 px-5 text-2xl font-roboto-mono font-bold">
+        Your recent cards will be shown below. (Coming soon!)
+      </div>
+
+      {/* Footer */}
       <footer>
         <div className="mt-auto text-center py-5 font-roboto-mono">
           &copy; 2024 StudyFlash. AU Project.
