216868ebe2f5b74b05c1d04a6aaa4bdb6f851d25
tintin4303 <nyunt.tin4303@gmail.com>
2025-09-19 16:15:28 +0700

    change background to animation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,7 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        {children}
+        <div className="flex min-h-screen flex-col p-5 animated-bg">
+          <Navbar />
+          <main className="min-h-screen">{children}</main>
+          <Footer />
+        </div>
+
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    // Navbar
-    <div className="flex min-h-screen flex-col p-5 bg-gradient-to-b from-black via-blue-900 to-purple-900">
-        <div className="flex justify-between nav-bar px-5 glass-effect">
-          <div className="text-2xl font-roboto-mono">
-            <Link href="/">StudyFlash</Link>
-          </div>
-          <div className="flex gap-5">
-            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110 hidden sm:block">
-              <Link href="/flashcards">My Flashcards</Link>
-            </div>
-            <div className="nav-item glass-effect px-4 transition-transform duration-200 hover:scale-110">
-              <Link href="/practice">Practice</Link>
-            </div>
-          </div>
-        </div>
-
-      {/* Body */}
+    <>
       <div className="">
           <div className="px-5">
             <div className="text-4xl font-roboto-mono font-bold mb-5 mt-10">
@@ -34,17 +18,9 @@ export default function Home() {
           </div>
       </div>
 
-      {/* Recent Cards */}
       <div className="mt-15 px-5 text-2xl font-roboto-mono font-bold">
         Your recent cards will be shown below. (Coming soon!)
       </div>
-
-      {/* Footer */}
-      <footer>
-        <div className="mt-auto text-center py-5 font-roboto-mono">
-          &copy; 2024 StudyFlash. AU Project.
-        </div>
-      </footer>
-    </div>
+    </>
   );
 }
